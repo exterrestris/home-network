dd437452045fdcb96ea286a47d54baa945be3f99
Sean Ellingham <sean@terrestris.co.uk>
2021-03-13 14:02:41 +0000

    Configure WSL to allow linux permissions if not set

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ ! -e /etc/wsl.conf ]; then
+  sudo cp wsl.conf.default /etc/wsl.conf
+  DRIVES=$(wmic.exe logicaldisk get deviceid | cut -d: -f1 -s | tr '[:upper:]' '[:lower:]')
+  GID=$(id -g)
+  for drive in $DRIVES; do
+    sudo umount /mnt/$drive
+    sudo mount -t drvfs $drive: /mnt/$drive -o metadata,uid=$UID,gid=$GID,umask=22,fmask=111
+  done
+fi
 ./install-ansible.sh
 chmod 775 .
 make install-git-hooks
