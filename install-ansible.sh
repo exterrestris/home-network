733b502acdd360c37d1f0cbfe0392bd9f56618de
Sean Ellingham <sean@terrestris.co.uk>
2021-03-13 11:28:41 +0000

    Basic ansible install script

---
+++ install-ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DISTRO=$( awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' )
+if [ -z $DISTRO ]; then
+    echo "Unknown Linux distribtion"
+    exit 1
+fi
+
+case $DISTRO in
+    ubuntu)
+        apt-add-repository --yes --update ppa:ansible/ansible
+        apt install --yes python3 git make ansible-base
+        ;;
+    *)
+        echo "Unsupported Linux distribution: $DISTRO"
+        exit 1
+esac
