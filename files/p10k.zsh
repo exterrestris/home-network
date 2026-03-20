129c77a6b5c9266b6bbf0c4186176c37a45c54d7
Sean Ellingham <sean.ellingham@basecreative.co.uk>
2026-03-20 06:44:35 +0000

    Add OS version to prompt

--- files/p10k.zsh
+++ files/p10k.zsh
@@ -32,7 +32,8 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    os_icon                 # os identifier
+    # os_icon                 # os identifier
+    os_version              # custom os identifier with version
     context                 # user@hostname
     dir                     # current directory
     vcs                     # git status
@@ -1621,6 +1622,12 @@
   # Type `p10k help segment` for documentation and a more sophisticated example.
   function prompt_example() {
     p10k segment -b 1 -f 3 -i '⭐' -t 'hello, %n'
+  }
+
+ function prompt_os_version() {
+    os_version=$(lsb_release -r | awk '{print $2}')
+
+    p10k segment -b $POWERLEVEL9K_OS_ICON_BACKGROUND -f $POWERLEVEL9K_OS_ICON_FOREGROUND -i $_p9k_os_icon -t $os_version
   }
 
   # User-defined prompt segments may optionally provide an instant_prompt_* function. Its job
